bc07ad233ebd0459c6dde55ee26a88de40c238cb
Azimath <npnickster@gmail.com>
2021-10-24 13:43:07 -0400

    Add random retweet on days with no tweet

--- ohio_bot.py
+++ ohio_bot.py
@@ -2,7 +2,9 @@ import tweepy
 
 import pytz
 
-from datetime import datetime
+import random
+
+from datetime import datetime, timedelta
 import json
 
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -11,7 +13,22 @@ from apscheduler.triggers.cron import CronTrigger
 from apscheduler.executors.pool import ThreadPoolExecutor
 
 io_timezone = pytz.timezone("America/Toronto")
-io_tweet_filename = "filtered_io_tweets.txt"
+io_tweet_filename = "yes.txt"
+
+def pairwise(iterable): # Modified from https://stackoverflow.com/a/2315049
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a = iter(iterable)
+    b = iter(iterable)
+
+    next(b)
+    return zip(a, b)
+
+def missing_dates(dates): # https://stackoverflow.com/a/2315049
+    for prev, curr in pairwise(sorted(dates)):
+        i = prev
+        while i + timedelta(1) < curr:
+            i += timedelta(1)
+            yield i
 
 jobstores = {
     'default': MemoryJobStore()
@@ -32,6 +49,22 @@ def retweet(tweetid):
 
     api = tweepy.API(auth)
 
+    print("Retweeting {}".format(tweetid))
+
+    api.unretweet(tweetid)
+    #api.retweet(tweetid)
+
+def retweet_random(tweetids):
+    import tokens
+    auth = tweepy.OAuthHandler(tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET)
+    auth.set_access_token(tokens.ACCESS_TOKEN, tokens.ACCESS_SECRET)
+
+    api = tweepy.API(auth)
+
+    tweetid = random.choice(tweetids)
+
+    print("Retweeting {}".format(tweetid))
+    
     api.unretweet(tweetid)
     #api.retweet(tweetid)
 
@@ -39,6 +72,9 @@ if __name__ == "__main__":
     tweetdatetimes = []
     with open(io_tweet_filename, "r") as io_tweet_file:
         io_tweets = json.loads(io_tweet_file.read())
+
+        tweetdays = []
+        io_tweet_ids = []
         for tweet in io_tweets:
             tweetdatetime = datetime.strptime(tweet["date"], '%Y-%m-%dT%H:%M:%S%z')
             tweetEST = tweetdatetime.astimezone(tz=io_timezone)
@@ -46,5 +82,13 @@ if __name__ == "__main__":
             ct = CronTrigger(month=tweetEST.month, day=tweetEST.day, hour=tweetEST.hour, minute=tweetEST.minute, second=tweetEST.second)
             scheduler.add_job(retweet, trigger=ct, args=(tweet["id"],))
 
+            tweetdays.append(tweetEST)
+            io_tweet_ids.append(tweet["id"])
+
+    for missing in missing_dates(tweetdays):
+        ct = CronTrigger(month=missing.month, day=missing.day, hour=random.randint(6, 12), minute=random.randint(0, 59), second=random.randint(0, 59))
+        scheduler.add_job(retweet_random, trigger=ct, args=(io_tweet_ids,))
+
     scheduler.print_jobs()
+    print("Scheduled {} tweets!".format(len(scheduler.get_jobs())))
     scheduler.start()
