00fb1167f135cd6bc37365eb124986dd51397074
Azimath <npnickster@gmail.com>
2021-10-27 19:53:47 -0400

    Incorporate new dual mode markov generator

--- ohaio_bot.py
+++ ohaio_bot.py
@@ -22,7 +22,7 @@ job_defaults = {
 
 scheduler = BlockingScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=io_timezone)
 
-def tweet(first_words, word_dict):
+def tweet(first_words, word_dict_one, word_dict_two):
     import tokens
     auth = tweepy.OAuthHandler(tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET)
     auth.set_access_token(tokens.ACCESS_TOKEN, tokens.ACCESS_SECRET)
@@ -33,7 +33,10 @@ def tweet(first_words, word_dict):
 
     tweetText = ""
     while len(tweetText) < 280 and not "<|endoftext|>" in tweetText:
-        chain.append(random.choice(word_dict[(chain[-2], chain[-1])]))
+        if len(chain[-1]) == 1 and (chain[-2], chain[-1]) in word_dict_two.keys():
+            chain.append(random.choice(word_dict_two[(chain[-2], chain[-1])]))
+        else:
+            chain.append(random.choice(word_dict_one[chain[-1]]))
         tweetText = ' '.join(chain)
 
     tweetText = tweetText.replace("<|endoftext|>", '')
@@ -58,21 +61,28 @@ if __name__ == "__main__":
         for i in range(len(corpus)-2):
             yield (corpus[i], corpus[i+1], corpus[i+2])
             
-    #pairs = make_pairs(corpus)
+    pairs = make_pairs(corpus)
     trios = make_trios(corpus)
 
-    word_dict = {}
+    word_dict_one = {}
+    word_dict_two = {}
+
+    for word_1, word_2 in pairs:
+        if word_1 in word_dict_one.keys():
+            word_dict_one[word_1].append(word_2)
+        else:
+            word_dict_one[word_1] = [word_2]
 
     for word_1, word_2, word_3 in trios:
-        if (word_1, word_2) in word_dict.keys():
-            word_dict[(word_1, word_2)].append(word_3)
+        if (word_1, word_2) in word_dict_two.keys():
+            word_dict_two[(word_1, word_2)].append(word_3)
         else:
-            word_dict[(word_1, word_2)] = [word_3]
+            word_dict_two[(word_1, word_2)] = [word_3]
 
-    first_words = [w for w in list(word_dict) if w[0].startswith("O") or w[0].lower().startswith("good") or w[0].startswith("G")]
+    first_words = [w for w in list(word_dict_two) if w[0].startswith("O") or w[0].lower().startswith("good") or w[0].startswith("G")]
 
     ct = CronTrigger(hour=8, minute=0, second=0, jitter=60*30)
-    scheduler.add_job(tweet, trigger=ct, args=(first_words, word_dict))
+    scheduler.add_job(tweet, trigger=ct, args=(first_words, word_dict_one, word_dict_two))
 
     print("Starting scheduler!")
 
