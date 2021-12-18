025568d8cbf478b867a2f248b1d322c61d19592c
Azimath <npnickster@gmail.com>
2021-12-17 21:42:46 -0500

    Add emoji block markov chain

--- ohaio_markov.py
+++ ohaio_markov.py
@@ -1,54 +1,96 @@
 import random
+import emoji
 
 #https://towardsdatascience.com/simulating-text-with-markov-chains-in-python-1a27e6d13fc6
+
+def is_emojiblock(text):
+    if len(text) <= 1:
+        return False
+        
+    for character in text:
+        if character not in emoji.UNICODE_EMOJI_ENGLISH:
+            return False
+    return True
+
+def make_pairs(corpus):
+    for i in range(len(corpus)-1):
+        yield (corpus[i], corpus[i+1])
+
+def make_trios(corpus):
+    for i in range(len(corpus)-2):
+        yield (corpus[i], corpus[i+1], corpus[i+2])
+
+def mod_key(key):
+    if is_emojiblock(key):
+        return "<|emojiblock|>"
+    else:
+        return key.lower()
 
 def load_corpus(filepath):
     text = open(filepath, encoding='utf8').read()
 
     corpus = text.split()
 
-    def make_pairs(corpus):
-        for i in range(len(corpus)-1):
-            yield (corpus[i], corpus[i+1])
+    emojiCorpus = []
+    for i, t in enumerate(corpus):
+        if is_emojiblock(t):
+            corpus[i] = "<|emojiblock|>"
+            emojiCorpus.append(t)
 
-    def make_trios(corpus):
-        for i in range(len(corpus)-2):
-            yield (corpus[i], corpus[i+1], corpus[i+2])
-            
+    emojiCorpus = '\n'.join(emojiCorpus)
+    
     pairs = make_pairs(corpus)
     trios = make_trios(corpus)
+    emojiPairs = make_pairs(emojiCorpus)
 
     word_dict_one = {}
     word_dict_two = {}
+    emoji_dict_one = {}
 
     for word_1, word_2 in pairs:
-        word_1 = word_1.lower()
+        word_1 = mod_key(word_1)
         if word_1 in word_dict_one.keys():
             word_dict_one[word_1].append(word_2)
         else:
             word_dict_one[word_1] = [word_2]
 
     for word_1, word_2, word_3 in trios:
-        word_1 = word_1.lower()
-        word_2 = word_2.lower()
+        word_1 = mod_key(word_1)
+        word_2 = mod_key(word_2)
         if (word_1, word_2) in word_dict_two.keys():
             word_dict_two[(word_1, word_2)].append(word_3)
         else:
             word_dict_two[(word_1, word_2)] = [word_3]
 
+    for emoji_1, emoji_2 in emojiPairs:
+        if emoji_1 in emoji_dict_one.keys():
+            emoji_dict_one[emoji_1].append(emoji_2)
+        else:
+            emoji_dict_one[emoji_1] = [emoji_2]
+
     first_words = [w for w in list(word_dict_two) if w[0].lower().startswith("o") or w[0].lower().startswith("g")]
     random_word = [w for w in list(word_dict_one)]
 
-    return (first_words, random_word, word_dict_one, word_dict_two)
+    return (first_words, random_word, word_dict_one, word_dict_two, emoji_dict_one)
+
+def generate_emojiblock(emoji_dict):
+    emojiBlock = list(random.choice(emoji_dict['\n']))
+    while len(emojiBlock) < 6:
+        next = random.choice(emoji_dict[emojiBlock[-1]])
+        if next != '\n':
+            emojiBlock.append(next)
+        else:
+            break
+    return ''.join(emojiBlock)
 
 def generate_tweet(chain_data):
-    first_words, random_word, word_dict_one, word_dict_two = chain_data
+    first_words, random_word, word_dict_one, word_dict_two, emoji_dict_one = chain_data
     chain = list(random.choice(first_words))
 
     tweetText = ""
     while len(tweetText) < 280 and not "<|endoftext|>" in tweetText:
-        lastOne = chain[-1].lower()
-        lastTwo = chain[-2].lower()
+        lastOne = mod_key(chain[-1])
+        lastTwo = mod_key(chain[-2])
         if len(lastOne) <= 3 and (lastTwo, lastOne) in word_dict_two.keys():
             chain.append(random.choice(word_dict_two[(lastTwo, lastOne)]))
         else:
@@ -66,6 +108,11 @@ def generate_tweet(chain_data):
                 else:
                     chain.pop()
 
+        if chain[-1] == "<|emojiblock|>":
+            chain[-1] = generate_emojiblock(emoji_dict_one)
+        if chain[-2] == "<|emojiblock|>":
+            chain[-2] = generate_emojiblock(emoji_dict_one)
+        
         tweetText = ' '.join(chain)
 
     tweetText = tweetText.replace("<|endoftext|>", '')
